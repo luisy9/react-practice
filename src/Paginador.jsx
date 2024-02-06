9104a95a42d38c9bbce9e5043eaa95a2d7001b50
Luis de Haro <luisdeharo4@gmail.com>
2024-02-06 10:50:04 +0100

    pagination

--- src/Paginador.jsx
+++ src/Paginador.jsx
@@ -2,31 +2,52 @@ import { useState } from "react"
 
 const Paginador = () => {
 
-    const [sum, setSum] = useState([1, 2, 3, 4]);
+    const [sum, setSum] = useState(0);
+    const [nums, setNums] = useState([1, 2, 3, 4]);
+
     function sumQuantity() {
-        setSum([...sum, sum.length + 1]);
+        setSum(sum + 1);
+        // setSum([...sum, sum.length + 1]);
     }
 
     function resQuantity() {
+        setSum(sum - 1);
         //Solucionar esto
-        // const quitAnteriorNum = [...sum.filter(e => (sum[0] !== 1) || (sum[0 < 1]))];
         // console.log(quitAnteriorNum)
-        const filterSumState = [...sum.map(e => e - 1)];
-        if(sum.length > 4){
-            setSum(filterSumState);
-        }
+        // const filterSumState = [...sum.map(e => e - 1)];
+        // if (sum.length > 4) {
+        //     setSum(filterSumState);
+        // }
     }
 
     return (
         <div className="flex h-10 w-auto items-center">
             <button className="px-3 h-10 text-2xl" onClick={resQuantity}>-</button>
-            {sum.map((e, index) => {
+            { /* Con Bucle */}
+            {nums.map((e, index) => {
+                return (
+                    <>
+                        <div className="border-2 border-solid border-red-500 px-2">
+                            <button key={index}>{e}</button>
+                        </div>
+                    </>
+
+                )
+            })
+            }
+            { /* Sin Buble */}
+            {/* <div className="border-2 border-solid border-red-500 px-2">{sum + 1}</div>
+            <div className="border-2 border-solid border-red-500 px-2">{sum + 2}</div>
+            <div className="border-2 border-solid border-red-500 px-2">{sum + 3}</div>
+            <div className="border-2 border-solid border-red-500 px-2">{sum + 4}</div>
+            <div className="border-2 border-solid border-red-500 px-2">{sum + 5}</div> */}
+            {/* {sum.map((e, index) => {
                 return (
                     <div className="border-2 px-10 py-4 border-solid border-red-500" key={index}>
                         <p key={index}>{e}</p>
                     </div>
                 )
-            })}
+            })} */}
             <button className="px-3 h-10 text-2xl" onClick={sumQuantity}>+</button>
         </div>
     )
